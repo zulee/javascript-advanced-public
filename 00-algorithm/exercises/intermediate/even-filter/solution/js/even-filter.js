e4fa9050f5a528065b4e8ae680957b226f012266
szzn@nmhh <szzn@nmhh>
2022-10-18 10:16:37 +0200

    int-numbersortes

--- 00-algorithm/exercises/intermediate/even-filter/solution/js/even-filter.js
+++ 00-algorithm/exercises/intermediate/even-filter/solution/js/even-filter.js
@@ -1,1 +1,14 @@
 export default getTheEvenElements;
+
+function getTheEvenElements(arr)
+{
+    let resArr = [];
+
+    arr.forEach(element => {
+        if (element % 2 === 0) {
+            resArr.push(element);
+        }
+    });
+
+    return resArr;
+}
--- 00-algorithm/exercises/intermediate/number-string-sorter/solution/js/number-string-sorter.js
+++ 00-algorithm/exercises/intermediate/number-string-sorter/solution/js/number-string-sorter.js
@@ -1,1 +1,15 @@
 export default numberAndStringSorter;
+
+function numberAndStringSorter(arr)
+{
+    let strArr = arr.filter(e => typeof e === 'string');
+    let intArr = arr.filter(e => typeof e === 'number');
+    let resArr = [];
+
+    for (let index = 0; index < strArr.length; index++) {
+        resArr.push(intArr[index]);
+        resArr.push(strArr[index]);
+    }
+
+    return resArr;
+}
