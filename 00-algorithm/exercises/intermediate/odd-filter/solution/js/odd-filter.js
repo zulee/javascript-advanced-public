52b2b2e8fc6a7ea60f7a09bc81d9700457d86e15
szzn@nmhh <szzn@nmhh>
2022-10-19 10:18:19 +0200

    intermediate tests - pass

--- 00-algorithm/exercises/intermediate/odd-filter/solution/js/odd-filter.js
+++ 00-algorithm/exercises/intermediate/odd-filter/solution/js/odd-filter.js
@@ -1,1 +1,6 @@
 export default getOddEvenElements;
+
+function getOddEvenElements(arr)
+{
+    return arr.filter(e =>e % 2 !== 0);
+}
--- 00-algorithm/exercises/intermediate/sort-by-asc/solution/js/sort-by-asc.js
+++ 00-algorithm/exercises/intermediate/sort-by-asc/solution/js/sort-by-asc.js
@@ -1,1 +1,6 @@
 export default sortByAsc;
+
+function sortByAsc(arr)
+{
+    return arr.sort((a,b) => a-b);
+}
